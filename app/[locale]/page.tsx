d57505fd2bd485ce5a06d5ad1d220f6932fd0e99
lorenzo.cirone <lorenzo.cirone5bi@gmail.com>
2026-08-29 17:52:26 +0200

    modificata hero e sezione corazon napoletano

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,12 @@
 import { setRequestLocale } from "next-intl/server"
 
 import { Hero } from "@/components/sections/home/hero"
-import { Corazon } from "@/components/sections/home/corazon"
 import { Capabilities } from "@/components/sections/home/capabilities"
 import { Positioning } from "@/components/sections/home/positioning"
-import { Technology } from "@/components/sections/home/technology"
 import { Process } from "@/components/sections/home/process"
 import { GranCanaria } from "@/components/sections/home/gran-canaria"
+import { Corazon } from "@/components/sections/home/corazon"
+import { Technology } from "@/components/sections/home/technology"
 import { FinalCta } from "@/components/sections/home/final-cta"
 
 type Props = {
@@ -20,12 +20,12 @@ export default async function HomePage({ params }: Props) {
   return (
     <>
       <Hero />
-      <Corazon />
       <Capabilities />
       <Positioning />
-      <Technology />
       <Process />
       <GranCanaria />
+      <Corazon />
+      <Technology />
       <FinalCta />
     </>
   )
